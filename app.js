ebf3d35ceab82b40344bb65ab5728873dad84781
Nitin Jain <jainnitin198953@gmail.com>
2021-06-17 21:27:16 +0530

    validation by putting reuired and min/max

--- app.js
+++ app.js
@@ -8,24 +8,32 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB", {        //At the end, na
 });
 
 const personSchema = new mongoose.Schema({      // schema of the data you want to create
-    name: String,
-    age: Number
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 5,
+        max: 30
+    }
 });
 
 const Person = mongoose.model("person", personSchema);         //defining a model --> convert singular to plural
 
-const Nitin = new Person({              //Adding new content
-    name: "nitin",
-    age: 21
-});
-const Kirti = new Person({
-    name: "kirti",
-    age: 22
-});
-const Himani = new Person({
-    name: "himani",
-    age: 15
-});
+// const Nitin = new Person({              //Adding new content
+//     name: "nitin",
+//     age: 21
+// });
+// const Kirti = new Person({
+//     name: "kirti",
+//     age: 22
+
+// });
+// const Himani = new Person({
+//     name: "himani",
+//     age: 15
+// });
 
 // Person.insertMany([Nitin, Kirti, Himani], function (err) {           //To insert in the database
 //     if (err) {
@@ -35,6 +43,12 @@ const Himani = new Person({
 //     }
 
 // });
+
+const Mukul = new Person({
+    age: 25
+});
+
+Person.save();
 
 Person.find(function (err, people) {                //To Find all the people
     if (err) {
